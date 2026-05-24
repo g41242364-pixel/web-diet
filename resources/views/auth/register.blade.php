f41143a0bd57a8bb9c1c320daf6ddbb9cf084a08
rifa29317 <g41242076@student.polije.ac.id>
2026-05-24 17:54:31 +0700

    Update register.blade.php

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -300,7 +300,7 @@
                                 <path
                                     d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z" />
                             </svg>
-                            <input type="text" name="name" value="{{ old('name') }}" required>
+                            <input type="text" name="name" value="{{ old('name') }}" placeholder="Nama lengkap Anda" required>
                         </div>
                     </div>
 
@@ -323,7 +323,7 @@
                                 <path
                                     d="M12.65 10C11.83 7.67 9.61 6 7 6c-3.31 0-6 2.69-6 6s2.69 6 6 6c2.61 0 4.83-1.67 5.65-4H17v4h4v-4h2v-4H12.65zM7 14c-1.1 0-2-.9-2-2s.9-2 2-2 2 .9 2 2-.9 2-2 2z" />
                             </svg>
-                            <input type="password" name="password" required>
+                            <input type="password" name="password" placeholder="Minimal 8 karakter" required>
                         </div>
                     </div>
 
@@ -334,7 +334,7 @@
                                 <path
                                     d="M12.65 10C11.83 7.67 9.61 6 7 6c-3.31 0-6 2.69-6 6s2.69 6 6 6c2.61 0 4.83-1.67 5.65-4H17v4h4v-4h2v-4H12.65zM7 14c-1.1 0-2-.9-2-2s.9-2 2-2 2 .9 2 2-.9 2-2 2z" />
                             </svg>
-                            <input type="password" name="password_confirmation" required>
+                            <input type="password" name="password_confirmation" placeholder="Ulangi password Anda" required>
                         </div>
                     </div>
 
