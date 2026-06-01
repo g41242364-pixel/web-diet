43da43c93af1d65c817e12b330429f767335a349
g41242364-pixel <g41242364@student.polije.ac.id>
2026-06-02 02:07:22 +0700

    Update detail_artikel.blade.php

--- resources/views/pengguna/detail_artikel.blade.php
+++ resources/views/pengguna/detail_artikel.blade.php
@@ -5,20 +5,23 @@
 @section('content')
     <link rel="stylesheet" href="{{ asset('assets/css/pengguna/detail_artikel.css') }}">
 
+    <a href="{{ route('pengguna.artikel') }}" class="btn-back">
+        ← Kembali ke Artikel
+    </a>
+
     <div class="article-header">
         <div class="header-info">
             <div class="header-icon">
                 <svg width="30" height="30" viewBox="0 0 24 24" fill="#4285F4">
                     <path
-                        d="M4 6H2v14c0 1.1.9 2 2 2h14v-2H4V6zm16-4H8c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2zm-1 9H9V9h10v2zm-4 4H9v-2h6v2zm4-8H9V5h10v2z" />
+                        d="M4 6H2v14c0 1.1.9 2 2 2h14v-2H4V6zm16-4H8c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h12c0-1.1.9-2 2-2V4c0-1.1-.9-2-2-2zm-1 9H9V9h10v2zm-4 4H9v-2h6v2zm4-8H9V5h10v2z" />
                 </svg>
             </div>
             <div class="header-text">
-                <h2>Artikel Edukasi</h2>
-                <p>Bacaan ringkas seputar nutrisi & gaya hidup sehat</p>
-            </div>
+    <h2>TEST ARTIKEL EDUKASI</h2>
+    <p>Bacaan ringkas seputar nutrisi & gaya hidup sehat</p>
+</div>
         </div>
-        <a href="{{ route('pengguna.artikel') }}" class="btn-back">Kembali ke Artikel</a>
     </div>
 
     <div class="detail-wrapper">
