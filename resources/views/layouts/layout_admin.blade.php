514fc5bbdeff202b9541de5754d7feca61b117aa
g41242364-pixel <g41242364@student.polije.ac.id>
2026-06-01 23:16:14 +0700

    Update layout_admin.blade.php

--- resources/views/layouts/layout_admin.blade.php
+++ resources/views/layouts/layout_admin.blade.php
@@ -52,7 +52,7 @@
                         <line x1="8" y1="14" x2="16" y2="14" />
                         <line x1="8" y1="18" x2="16" y2="18" />
                     </svg>
-                    <span>Skrining IMT</span>
+                    <span>Skrining Diet</span>
                 </a>
             </li>
             <li>
