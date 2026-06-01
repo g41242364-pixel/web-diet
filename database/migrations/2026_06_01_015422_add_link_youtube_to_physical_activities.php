7593bf024e999754bae6aab3624d6889fe17bd16
g41242050-ops <g41242050@student.polije.ac.id>
2026-06-01 14:49:48 +0700

    Update 2026_06_01_015422_add_link_youtube_to_physical_activities.php

--- database/migrations/2026_06_01_015422_add_link_youtube_to_physical_activities.php
+++ database/migrations/2026_06_01_015422_add_link_youtube_to_physical_activities.php
@@ -9,7 +9,11 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('physical_activities', function (Blueprint $table) {
-            $table->dropColumn('gambar');
+
+            if (Schema::hasColumn('physical_activities', 'gambar')) {
+                $table->dropColumn('gambar');
+            }
+
             $table->string('link_youtube')->nullable()->after('lokasi');
         });
     }
@@ -17,7 +21,11 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('physical_activities', function (Blueprint $table) {
-            $table->dropColumn('link_youtube');
+
+            if (Schema::hasColumn('physical_activities', 'link_youtube')) {
+                $table->dropColumn('link_youtube');
+            }
+
             $table->string('gambar')->nullable()->after('lokasi');
         });
     }
